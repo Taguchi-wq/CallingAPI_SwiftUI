65614dfa2dc3c5a7aea400a9c7fc4b5f10acd4bc
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-08 17:52:30 +0900

    first commit

---
+++ CallingAPI/CallingAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallingAPIApp.swift
+//  CallingAPI
+//
+//  Created by cmStudent on 2021/11/08.
+//
+
+import SwiftUI
+
+@main
+struct CallingAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
