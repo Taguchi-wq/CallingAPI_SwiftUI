ac0ee6b47a30117ba4f76b8dee15938eab168448
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-08 18:43:27 +0900

    ListのスタイルをPlainListStyleに変更

--- CallingAPI/View/ContentView/ContentView.swift
+++ CallingAPI/View/ContentView/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Courses")
+            .listStyle(PlainListStyle())
             .onAppear { viewModel.fetchCourses() }
         }
     }
