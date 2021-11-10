c4d1bb1b51d6e955f23a48c1eb85686b260ca960
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-10 13:47:50 +0900

    ContentViewにコメントを追加

--- CallingAPI/View/ContentView/ContentView.swift
+++ CallingAPI/View/ContentView/ContentView.swift
@@ -9,19 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // MARK: - Property Wrappers
+    /// ContentViewModel
     @StateObject private var viewModel = ContentViewModel()
     
+    
+    // MARK: - Body
     var body: some View {
         NavigationView {
             List {
                 ForEach(viewModel.courses, id: \.self) { course in
                     CourseView(name: course.name, image: course.image)
-                }
-            }
+                } // ForEach
+            } // List
             .navigationTitle("Courses")
             .listStyle(PlainListStyle())
             .onAppear { viewModel.fetchCourses() }
-        }
+        } // NavigationView
     }
 }
 
